7d531ef4a91d7156f6ab0c3e245234699dff111a
nithyaravi8198-tech <nithyaravi8198@gmail.com>
2026-04-11 00:16:27 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Enable Swagger
-app.UseSwagger();
-app.UseSwaggerUI();
-
-// Map controllers
-app.MapControllers();
-
-// Root endpoint (IMPORTANT)
-app.MapGet("/", () => "DevOps App Running 🚀");
+app.MapGet("/", () => "WORKING SUCCESS 🚀");
 
 app.Run();
